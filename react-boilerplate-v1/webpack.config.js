aa81604c3cacd449d550287260efdaf5fe7a57cc
la785m <lalvesdasilva@wayfair.com>
2021-02-18 16:51:20 +0100

    initial commit with boilerplate version 1 for react apps

---
+++ react-boilerplate-v1/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        path: path.join(__dirname, "public"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            cacheDirectory: true,
+                            babelrc: false,
+                            presets: [
+                                "@babel/env",
+                                "@babel/react"
+                            ],
+                            plugins: [
+                                "@babel/plugin-proposal-object-rest-spread",
+                                "syntax-optional-chaining",
+                                "@babel/plugin-proposal-class-properties"
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    devtool: "eval-cheap-module-source-map",
+    devServer: {
+        contentBase: path.join(__dirname, "public")
+    }
+}
